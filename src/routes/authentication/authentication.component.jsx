998320799b154add852670d8978d9dbd0f9ae965
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-10-06 00:03:37 +0200

    creating authentication

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -8,8 +8,9 @@ import {
 } from '../../utilities/firebase/firebase.utilities';
 
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 
-const SignIn = () => {
+const Authentication = () => {
   /* useEffect(
     () => async () => {
       const response = await getRedirectResult(auth);
@@ -32,14 +33,14 @@ const SignIn = () => {
 
   return (
     <div>
-      <h1>sign in page</h1>
-      <button onClick={logGoogleUser}>Sign on with Google Popup</button>
+      {/* <button onClick={logGoogleUser}>Sign on with Google Popup</button> */}
       {/*  <button onClick={signInWithGoogleRedirect}>
         Sign on with Google Redirect
       </button> */}
+      <SignInForm />
       <SignUpForm />
     </div>
   );
 };
 
-export default SignIn;
+export default Authentication;
