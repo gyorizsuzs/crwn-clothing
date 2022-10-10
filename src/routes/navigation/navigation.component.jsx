f71dc0a9d73c7221900e3c50c583f1a6be07bc54
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-10-11 01:08:26 +0200

    creating shop page with dummy products, cart container with dropdown cart

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,7 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
-
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { ReactComponent as CrwLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
 
@@ -35,7 +36,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
--- src/utilities/firebase/firebase.utilities.js
+++ src/utilities/firebase/firebase.utilities.js
@@ -88,5 +88,5 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 export const signOutUser = async () => await signOut(auth);
 
 export const onAuthStateChangedListener = (callback) => {
-  onAuthStateChanged(auth, callback, errorCallback, completeCallback);
+  onAuthStateChanged(auth, callback);
 };
