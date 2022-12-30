1a620f3d85186143b857b48f386e532592690e67
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-12-30 17:23:39 +0100

    setting up reselect

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,9 +1,29 @@
-export const selectCategoriesMap = (state) => {
-  console.log('selector fired');
+import { createSelector } from 'reselect';
 
-  return state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+const selectCategoryReducer = (state) => {
+  console.log('selector 1 fired');
+
+  return state.categories;
 };
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer], // input
+  (categoriesSlice) => {
+    console.log('selector 2 fired');
+
+    return categoriesSlice.categories; // output
+  }
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => {
+    console.log('selector 3 fired');
+
+    return categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+  }
+);
